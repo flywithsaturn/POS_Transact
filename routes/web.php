a16ac833b9350099c5a89284281684871c7efca9
Satriowish <satriowisnuap@gmail.com>
2025-03-08 22:52:25 +0700

    Membuat Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Halaman Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Halaman Products dengan prefix
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+});
+
+// Halaman User dengan parameter
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Halaman Penjualan
+Route::get('/sales', [SalesController::class, 'index']);
