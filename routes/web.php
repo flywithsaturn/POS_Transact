8051497a70a5185a92f9ba5ef5fa5458911a930d
Satriowish <satriowisnuap@gmail.com>
2025-03-15 16:46:57 +0700

    Jobsheet 3 Praktikum 5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -12,6 +13,7 @@ Route::get('/', function () {
 });
 
 Route::get('/level',[LevelController::class,'index']);
+Route::get('/kategori',[KategoriController::class,'index']);
 
 // // Halaman Home
 // Route::get('/', [HomeController::class, 'index']);
