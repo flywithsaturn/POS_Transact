2d2704fc5e71ade36cd6b6855d1eaf616ceba7e3
Satriowish <satriowisnuap@gmail.com>
2025-03-15 22:21:59 +0700

    Jobsheet 4 Praktikum 2.4 No 8-10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,10 +12,13 @@ class UserController extends Controller
     {
        $user = UserModel::firstOrNew(
         [
-            'username' => 'manager',
-            'nama' => 'Manageer',
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
         ]
         );
+        $user->save();
        return view('user',['data' => $user]);
     }
 }
