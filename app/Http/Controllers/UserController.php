1583f8eac55d6f129f6489669038c9c7012b3d9d
Satriowish <satriowisnuap@gmail.com>
2025-03-15 22:26:48 +0700

    Jobsheet 4 Praktikum 2.5 No 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,32 @@ class UserController extends Controller
 {
     public function index()
     {
-       $user = UserModel::firstOrNew(
-        [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
             'password' => Hash::make('12345'),
-            'level_id' => 2
-        ]
-        );
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
+        $user->save();
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+
+        dd($user->isDirty());
+
         $user->save();
        return view('user',['data' => $user]);
     }
@@ -60,3 +78,12 @@ class UserController extends Controller
         //Praktikum Jobsheet 4
         //   $user = UserModel::where('level_id', 2)->count(); //untuk menghitung banyaknya data yang muncul dengan level id 2
       
+        //Praktikum Jobsheet 4
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        //     );
