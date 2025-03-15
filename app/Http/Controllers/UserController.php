ea4d71c716fec1762ed3f8c5ec5fce3f359d0f79
Satriowish <satriowisnuap@gmail.com>
2025-03-15 17:28:23 +0700

    Jobsheet 3 Praktikum 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function show($id, $name)
+    public function index()
     {
-        return view('user', compact('id', 'name'));
+        //    // tambah data user dengan Eloquent Model
+        //    $validLevelId = \DB::table('m_level')->pluck('level_id')->first(); // Ambil level_id pertama yang tersedia
+        //    $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => $validLevelId
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
+        // // akses userMOdel
+        // $user = UserModel::all(); //ambil semua data dari table m_user
+        // return view('user', ['data' => $user]);
+
+        $data = [
+            'nama' => 'Pelanggan pertama',
+        ];
+        UserModel::where('username','customer-1')->update($data); 
+        $user = UserModel::all(); //ambil semua data dari table m_user
+        return view('user', ['data' => $user]);
     }
-}
+    }
+
