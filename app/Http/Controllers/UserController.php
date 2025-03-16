e53a25eef6bd503f5a6a2e58f5b86ac351efdfee
Satriowish <satriowisnuap@gmail.com>
2025-03-16 10:26:03 +0700

    Jobsheet 4 Praktikum 2.6 No 1-2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,22 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-
-        $user->username = 'manager12';
-
-        $user->save();
-
-         $user->wasChanged(); // true
-         $user->wasChanged('username'); // true
-         $user->wasChanged(['username', 'level_id']); // true
-         $user->wasChanged('nama'); // false
-         dd($user->wasChanged(['nama', 'username'])); // true
+       $user = UserModel::all();
+       return view('user',['data' => $user]);
     }
 }
 
@@ -97,3 +83,21 @@ class UserController extends Controller
         // dd($user->isDirty());
 
         // $user->save();
+
+        // Praktikum 2.5 jobsheet 4
+        // $user = UserModel::create([
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager11',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager12';
+
+        // $user->save();
+
+        //  $user->wasChanged(); // true
+        //  $user->wasChanged('username'); // true
+        //  $user->wasChanged(['username', 'level_id']); // true
+        //  $user->wasChanged('nama'); // false
+        //  dd($user->wasChanged(['nama', 'username'])); // true
