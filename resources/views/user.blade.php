0b615015604e5edcd63e30fca21f62632d9a0bd1
Satriowish <satriowisnuap@gmail.com>
2025-03-16 20:37:47 +0700

    Jobsheet 4 Praktikum 2.7 No 4-7

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,6 +12,8 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <th>Aksi</th>
         </tr>
 
@@ -22,6 +24,8 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->level_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
                 <td>
                     <a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> |
                     <a href="{{ url('/user/hapus/' . $d->user_id) }}" onclick="return confirm('Yakin ingin menghapus?')">Hapus</a>          
