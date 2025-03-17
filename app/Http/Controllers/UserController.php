148be114b3f895b91ac282084b049f6f23945518
Satriowish <satriowisnuap@gmail.com>
2025-03-17 20:39:46 +0700

    Jobsheet 5 Praktikum Ke-4 (Implementasi Filtering Database)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,13 +23,26 @@ class UserController extends Controller
 
         $activeMenu = 'user';
 
-        return view('user.index', compact('breadcrumb', 'page', 'activeMenu'));
+        $level = LevelModel::all(); //ambil data level untuk filter level
+
+        return view('user.index', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'level' => $level,
+            'activeMenu' => $activeMenu
+        ]);
+        
     }
 
     // Ambil data user dalam bentuk JSON untuk DataTables
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        // Filter berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             ->addIndexColumn()
