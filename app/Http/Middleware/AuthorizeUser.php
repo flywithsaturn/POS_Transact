21669a649c1dde92c0f1fc129f8b338fd11a9cfa
Satriowish <satriowisnuap@gmail.com>
2025-05-12 21:34:27 +0700

    Jobsheet 7 Praktikum 3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,15 +13,15 @@ class AuthorizeUser
      *
      * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
         $user = $request->user(); // ambil data user yg login
 
         // fungsi user() diambil dari UserModel.php
-        if ($user->hasRole($role)) { // cek apakah user punya role yg diinginkan
-            return $next($request);
+        $user_role = $request->user()->getRole(); // ambil data level kode dari user yg login
+        if (in_array($user_role, $roles)) { // cek apakah role user ada di dalam array $roles
+            return $next($request); // jika role ada, maka lanjutkan ke route selanjutnya
         }
-
         // jika tidak punya role, maka tampilkan error 403
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
