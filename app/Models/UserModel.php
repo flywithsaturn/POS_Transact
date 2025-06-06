3a036cedeb9a7e88f46be07137858c783663db54
Satriowish <satriowisnuap@gmail.com>
2025-06-06 18:49:28 +0700

    Praktikum 1 Jobsheet 10 (API 1)

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,11 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user'; // nama table
     protected $primaryKey = 'user_id'; // primary key pada table tsb
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Api\RegisterController;
+
+Route::post('/register', RegisterController::class)->name('register');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
