72ec09832e0c27d5cc9c603635d3948e0ab7702a
Satriowish <satriowisnuap@gmail.com>
2025-06-06 20:44:33 +0700

    Praktikum 3 Jobsheet 10 (API 1)

--- routes/api.php
+++ routes/api.php
@@ -15,12 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
